4f0590cd959ad39c01957747aff7633129291c34
Ferious <36670574+Ferious@users.noreply.github.com>
2020-12-03 15:44:53 +0100

    Ready gui for applying algorithms
    Adjusted image loading path, for example: "pictures/1.bmp".
    Added buttons for selecting pictures from axes.
    Removed a useless code.

--- main/ProjectAIP.m
+++ main/ProjectAIP.m
@@ -1,28 +1,4 @@
 function varargout = ProjectAIP(varargin)
-% PROJECTAIP MATLAB code for ProjectAIP.fig
-%      PROJECTAIP, by itself, creates a new PROJECTAIP or raises the existing
-%      singleton*.
-%
-%      H = PROJECTAIP returns the handle to a new PROJECTAIP or the handle to
-%      the existing singleton*.
-%
-%      PROJECTAIP('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in PROJECTAIP.M with the given input arguments.
-%
-%      PROJECTAIP('Property','Value',...) creates a new PROJECTAIP or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before ProjectAIP_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to ProjectAIP_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
-
-% Edit the above text to modify the response to help ProjectAIP
-
-% Last Modified by GUIDE v2.5 26-Nov-2020 23:04:21
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -46,12 +22,6 @@ end
 
 % --- Executes just before ProjectAIP is made visible.
 function ProjectAIP_OpeningFcn(hObject, eventdata, handles, varargin)
-% This function has no output args, see OutputFcn.
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to ProjectAIP (see VARARGIN)
-
 % Choose default command line output for ProjectAIP
 handles.output = hObject;
 
@@ -63,42 +33,24 @@ guidata(hObject, handles);
 
 % --- Outputs from this function are returned to the command line.
 function varargout = ProjectAIP_OutputFcn(hObject, eventdata, handles) 
-% varargout  cell array for returning output args (see VARARGOUT);
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Get default command line output from handles structure
 varargout{1} = handles.output;
+
 % after start of program
 pause(0.1);
 fill_axises(1,hObject, eventdata,handles);
 
 function fill_axises(from,hObject, eventdata, handles)
-    array = ["@axes1_ButtonDownFcn";"@axes2_ButtonDownFcn";"@axes3_ButtonDownFcn";
-            "@axes4_ButtonDownFcn";"@axes5_ButtonDownFcn"];
-    for i = from:from+4
+   for i = from:from+4
        suffix = '.bmp';
        path = [num2str(i), suffix];
-       img = im2double(imread(path));
-       disp(i);
+       img = im2double(imread(['pictures/',path]));
        axHandle = (handles.(['axes',num2str(mod(i-1,5)+1)]));
        set(axHandle, 'UserData', img);
-       axesChild = imshow(img, 'Parent', axHandle);
-     %  set(axesChild, 'HitTest', 'off'); 
-     %  axis('off');
-       set(axesChild, 'ButtonDownFcn', array(mod(i-1,5)+1));
-     %  set(axesChild, 'ButtonDownFcn', {array(mod(i-1,5)+1), handles});
+       imshow(img, 'Parent', axHandle);
     end
 
 % --- Executes on slider movement.
 function imageSlider_Callback(hObject, eventdata, handles)
-% hObject    handle to imageSlider (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'Value') returns position of slider
-%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
 sliderValue = get(handles.imageSlider, 'Value');
 round = floor(sliderValue);
 fill_axises(round, hObject, eventdata, handles);
@@ -114,47 +66,53 @@ if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColo
     set(hObject,'BackgroundColor',[.9 .9 .9]);
 end
 
-% --- Executes on mouse press over axes background.
-function axes1_ButtonDownFcn(hObject, eventdata, handles)
-disp('calll');
-%orig = get(handles.axes1, 'UserData');
-%imshow(orig, 'Parent',handles.mainAxis);
-%set(handles.mainAxis, 'UserData', orig);
-
-% hObject    handle to axes1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-function axesImage_ButtonDownFcn(hObject, eventdata, handles)
-disp("calll 2");
-
-% --- Executes during object creation, after setting all properties.
-function axes1_CreateFcn(hObject, eventdata, handles)
-% Hint: place code in OpeningFcn to populate axes1
-
-% --- Executes on mouse press over axes background.
-function axes2_ButtonDownFcn(hObject, eventdata, handles)
-disp('axes2  was clicked');
-% hObject    handle to axes2 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+% --- Executes on button press in selectAxes1.
+function selectAxes1_Callback(hObject, eventdata, handles)
+    if ~isempty(handles.axes1)
+        imh = findobj(handles.axes1, 'type', 'image');
+        img = get(imh, 'CData');
+        imshow(img, 'Parent', handles.mainAxes);
+    else
+       warndlg('Incorect Input, Please Select An Image!','Warning');
+    end
 
 
-% --- Executes on mouse press over axes background.
-function axes3_ButtonDownFcn(hObject, eventdata, handles)
-% hObject    handle to axes3 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+% --- Executes on button press in selectAxes2.
+function selectAxes2_Callback(hObject, eventdata, handles)
+    if ~isempty(handles.axes2)
+        imh = findobj(handles.axes2, 'type', 'image');
+        img = get(imh, 'CData');
+        imshow(img, 'Parent', handles.mainAxes);
+    else
+       warndlg('Incorect Input, Please Select An Image!','Warning');
+    end
 
+% --- Executes on button press in selectAxes3.
+function selectAxes3_Callback(hObject, eventdata, handles)
+    if ~isempty(handles.axes3)
+        imh = findobj(handles.axes3, 'type', 'image');
+        img = get(imh, 'CData');
+        imshow(img, 'Parent', handles.mainAxes);
+    else
+       warndlg('Incorect Input, Please Select An Image!','Warning');
+    end
 
-% --- Executes on mouse press over axes background.
-function axes4_ButtonDownFcn(hObject, eventdata, handles)
-% hObject    handle to axes4 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+% --- Executes on button press in selectAxes4.
+function selectAxes4_Callback(hObject, eventdata, handles)
+    if ~isempty(handles.axes4)
+        imh = findobj(handles.axes4, 'type', 'image');
+        img = get(imh, 'CData');
+        imshow(img, 'Parent', handles.mainAxes);
+    else
+       warndlg('Incorect Input, Please Select An Image!','Warning');
+    end
 
-
-% --- Executes on mouse press over axes background.
-function axes5_ButtonDownFcn(hObject, eventdata, handles)
-% hObject    handle to axes5 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
+% --- Executes on button press in selectAxes5.
+function selectAxes5_Callback(hObject, eventdata, handles)
+    if ~isempty(handles.axes5)
+        imh = findobj(handles.axes5, 'type', 'image');
+        img = get(imh, 'CData');
+        imshow(img, 'Parent', handles.mainAxes);
+    else
+       warndlg('Incorect Input, Please Select An Image!','Warning');
+    end
